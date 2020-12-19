81ea8d86d33b343d07caf99b3f736049a36f150c
wlstj <wlstjd146@naver.com>
2020-12-19 19:08:25 +0900

    initial commit

--- study/print/Mystudy.py
+++ study/print/Mystudy.py
@@ -1,3 +1,20 @@
-a = 3
-b = 4
-print(a+b)
+a = "jeongmincheol"
+print(a)
+
+a = 4
+if a > 1:
+    print("a is good")
+
+a = 4
+if a > 5:
+    print("a is good")
+
+for a in [1,2,3]:
+    print(a)
+
+i = 0
+while i < 6:
+    i=i+1
+    print(i)
+
+#함수
