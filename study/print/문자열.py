a3aff178949727ed843295f132c2629d8ea6f256
wlstj <wlstjd146@naver.com>
2020-12-19 19:25:25 +0900

    initial commit

--- study/print/문자열.py
+++ study/print/문자열.py
@@ -1,3 +1,17 @@
 #문자열 ""로 둘러싸여 있으면 모두 문자열
-a = "Hello world"
-print(a)
+#a = "Hello world"
+#print(a)
+
+#food = "python favorite food is per1"
+#print(food)
+
+#food = 'python's favorite food is per1'
+#print(food)
+
+#문자열에 큰따옴표 포함시키기
+#say = '"Python is very easy." he says.'
+#print(say)
+
+#백슬래시를 사용해서 '와 "를 문자열에포함시키기
+food = 'Python\'s favorite food is per1'
+say = "\"Python is very easy.\" he says."
