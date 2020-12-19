e1451175def4beb7c98078e8a2fb70f48aa2fe7c
wlstj <wlstjd146@naver.com>
2020-12-19 19:26:13 +0900

    initial commit

--- study/print/문자열.py
+++ study/print/문자열.py
@@ -2,15 +2,16 @@
 #a = "Hello world"
 #print(a)
 
-#food = "python favorite food is per1"
-#print(food)
+food = "python favorite food is per1"
+print(food)
+
 
 #food = 'python's favorite food is per1'
 #print(food)
 
 #문자열에 큰따옴표 포함시키기
-#say = '"Python is very easy." he says.'
-#print(say)
+say = '"Python is very easy." he says.'
+print(say)
 
 #백슬래시를 사용해서 '와 "를 문자열에포함시키기
 food = 'Python\'s favorite food is per1'
